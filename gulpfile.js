4337dfcc2df267b56794e975f1e610832eb6171d
IamVeideR <IamVeideR@gmail.com>
2019-03-20 12:18:38 +0600

    loading

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+ 
+global.$ = {
+    gulp: require('gulp'),
+    gp: require ('gulp-load-plugins')(),
+    bs: require ('browser-sync').create(),
+    path: {
+        tasks: require('./template-2/config/tasks.js')
+    }
+}; 
+
+$.path.tasks.forEach(function (taskPath) {
+    require(taskPath) ();
+});
+
+$.gulp.task('default', $.gulp.series(
+    $.gulp.parallel('pug','sass','scripts:lib','scripts'),
+    $.gulp.parallel('watch','serve')
+)); 
---
+++ template-2/config/tasks.js
@@ -0,0 +1,7 @@
+module.exports = [
+    './template-2/tasks/pug',
+    './template-2/tasks/sass',
+    './template-2/tasks/serve',
+    './template-2/tasks/watch',
+    './template-2/tasks/script'
+];
